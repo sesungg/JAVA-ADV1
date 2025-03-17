7aeb68904f1d56b1ac7ede5fee248416ef2cd45c
sgseong <sgseong@gmail.com>
2025-03-17 21:23:16 +0900

    로그 만들기 및 여러 Thread 생성하기

---
+++ src/thread/util/MyLoggerMain.java
@@ -0,0 +1,10 @@
+package thread.util;
+
+import static thread.util.MyLogger.*;
+
+public class MyLoggerMain {
+    public static void main(String[] args) {
+        log("Hello World");
+        log(123);
+    }
+}
